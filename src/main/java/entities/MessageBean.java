2c7dfb50b45e4951fcb5b18b52279e8afd221be2
emiliantolo <emiliantolo@gmail.com>
2019-01-29 16:02:01 +0100

    chat

---
+++ src/main/java/entities/MessageBean.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Emiliano
+ */
+public class MessageBean {
+
+    private int Id;    
+    private String Email;
+    private int Message;
+    private String Date;
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    /**
+     * @return the Message
+     */
+    public int getMessage() {
+        return Message;
+    }
+
+    /**
+     * @param Message the Message to set
+     */
+    public void setMessage(int Message) {
+        this.Message = Message;
+    }
+
+    /**
+     * @return the Date
+     */
+    public String getDate() {
+        return Date;
+    }
+
+    /**
+     * @param Date the Date to set
+     */
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    /**
+     * @return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    
+    
+}
---
+++ src/main/webapp/chat.jsp
@@ -0,0 +1,34 @@
+<%-- 
+    Document   : chat
+    Created on : 29-gen-2019, 11.24.03
+    Author     : Emiliano
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <script src="js/chat.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+
+        <div id="chatdiv"></div>
+        <br>
+        <br>
+        <br>
+
+<select id="messages">
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>        
+    <option>5</option>    
+</select>
+        <button id="send">Send</button>
+        
+    </body>
+</html>
