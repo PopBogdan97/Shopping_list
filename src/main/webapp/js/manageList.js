fc2aacd4bb4191f28a9e5a45ac8ebe6d2576c4f8
PopBogdan97 <vasile.pop@studenti.unitn.it>
2019-01-31 00:46:52 +0100

    fix image on edit list

--- src/main/webapp/js/manageList.js
+++ src/main/webapp/js/manageList.js
@@ -42,7 +42,7 @@ function loadList() {
                             }
                         });
                         var html = '<div id="' + obj.id + '"style="display: flex" class="add-stop">' +
-                                '<img alt="" class="rounded float-left col-lg-1" style="background-color: darkgray;margin-right: 15px;padding: 5px;padding-bottom: 2px;padding-top: 2px;" src="#">' +
+                                '<img alt="" class="rounded float-left col-lg-1" style="background-color: darkgray;margin-right: 15px;padding: 5px;padding-bottom: 2px;padding-top: 2px; max-height: 50px;" src="#">' +
                                 '<div class="list-group col-lg">' +
                                 '<a class="list-group-item list-group-item-dark">' + obj.text + '</a>' +
                                 '</div>' +
@@ -72,7 +72,7 @@ function loadList() {
                             }
                         });
                         var html = '<div id="' + obj.id + '"style="display: flex">' +
-                                '<img alt="" class="rounded float-left col-lg-1" style="background-color: darkgray;margin-right: 15px;padding: 5px;padding-bottom: 2px;padding-top: 2px;" src="#">' +
+                                '<img alt="" class="rounded float-left col-lg-1" style="background-color: darkgray;margin-right: 15px;padding: 5px;padding-bottom: 2px;padding-top: 2px; max-height: 50px;" src="#">' +
                                 '<div class="list-group col-lg-8">' +
                                 '<a class="list-group-item list-group-item-dark">' + obj.text + '</a>' +
                                 '</div>' +
@@ -111,7 +111,7 @@ function loadList() {
                                     }
                                 });
                                 var html = '<div id="' + obj.ListId + '"style="display: flex">' +
-                                        '<img alt="" class="rounded float-left col-lg-1" style="background-color: darkgray;margin-right: 15px;padding: 5px;padding-bottom: 2px;padding-top: 2px;" src="#">' +
+                                        '<img alt="" class="rounded float-left col-lg-1" style="background-color: darkgray;margin-right: 15px;padding: 5px;padding-bottom: 2px;padding-top: 2px; max-height: 50px;" src="#">' +
                                         '<div class="list-group col-lg-8">' +
                                         '<a class="list-group-item list-group-item-dark">' + obj.ListName + '</a>' +
                                         '</div>' +
